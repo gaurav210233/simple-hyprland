c7d176308237dc02bf43d87712c28d75b73b46d3
Gaurav Bankar <gaurav210233@gmail.com>
2024-08-08 09:58:45 +0530

    command fixes

--- scripts/installer/hypr.sh
+++ scripts/installer/hypr.sh
@@ -11,14 +11,14 @@ print_info "\nStarting hypr setup..."
 print_info "\nEverything is recommended to INSTALL"
 
 run_command "pacman -S --noconfirm hyprland" "Install Hyprland (Must)" "yes"
-run_command "cp ~/simple-hyprland/configs/hypr/hyprland.conf ~/.config/hypr/" "Copy hyprland config (Must)" "yes" #no
+run_command "cp /home/$SUDO_USER/simple-hyprland/configs/hypr/hyprland.conf /home/$SUDO_USER/.config/hypr/" "Copy hyprland config (Must)" "yes" #no
 
 run_command "pacman -S --noconfirm xdg-desktop-portal-hyprland" "Install XDG desktop portal for Hyprland" "yes"
 
 run_command "pacman -S --noconfirm polkit-kde-agent" "Install KDE Polkit agent for authentication dialogs" "yes"
 
 run_command "pacman -S --noconfirm dunst" "Install Dunst notification daemon" "yes"
-run_command "cp -r ~/simple-hyprland/configs/dunst ~/.config/" "Copy dunst config" "yes"
+run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/dunst /home/$SUDO_USER/.config/" "Copy dunst config" "yes" 
 
 run_command "pacman -S --noconfirm qt5-wayland qt6-wayland" "Install QT support on wayland" "yes"
 
--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -10,27 +10,27 @@ log_message "Installation started for utilities section"
 print_info "\nStarting utilities setup..."
 
 run_command "pacman -S --noconfirm waybar" "Install Waybar - Status Bar" "yes"
-run_command "cp -r ~/simple-hyprland/configs/waybar ~/.config/" "Copy Waybar config" "yes"
+run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/waybar /home/$SUDO_USER/.config/" "Copy Waybar config" "yes"
 
 run_command "yay -S --sudoloop --noconfirm tofi" "Install Tofi - Application Launcher" "yes" "no"
-run_command "cp -r ~/simple-hyprland/configs/tofi ~/.config/" "Copy Tofi config(s)" "yes"
+run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/tofi /home/$SUDO_USER/.config/" "Copy Tofi config(s)" "yes"
 
 run_command "pacman -S --noconfirm cliphist" "Install Cliphist - Clipboard Manager" "yes"
 
 run_command "yay -S --sudoloop --noconfirm swww" "Install SWWW for wallpaper management" "yes" "no"
-run_command "cp -r ~/simple-hyprland/assets/backgrounds ~/.config/assets/backgrounds/" "Copy sample wallpapers to assets directory (Recommended)" "yes"
+run_command "cp -r /home/$SUDO_USER/simple-hyprland/assets/backgrounds /home/$SUDO_USER/.config/assets/backgrounds/" "Copy sample wallpapers to assets directory (Recommended)" "yes"
 
 run_command "yay -S --sudoloop --noconfirm hyprpicker" "Install Hyprpicker - Color Picker" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm hyprlock" "Install Hyprlock - Screen Locker (Must)" "yes" "no"
-run_command "cp ~/simple-hyprland/configs/hypr/hyprlock.conf ~/.config/hypr/" "Copy Hyprlock config" "yes"
+run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/" "Copy Hyprlock config" "yes"
 
 run_command "yay -S --sudoloop --noconfirm wlogout" "Install Wlogout - Session Manager" "yes" "no"
-run_command "cp -r ~/simple-hyprland/configs/wlogout ~/.config/ && cp -r ~/simple-hyprland/assets/wlogout ~/.config/assets/" "Copy Wlogout config and assets" "yes"
+run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/wlogout /home/$SUDO_USER/.config/ && cp -r /home/$SUDO_USER/simple-hyprland/assets/wlogout /home/$SUDO_USER/.config/assets/" "Copy Wlogout config and assets" "yes"
 
 run_command "yay -S --sudoloop --noconfirm grimblast" "Install Grimblast - Screenshot tool" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm hypridle" "Install Hypridle for idle management (Must)" "yes" "no"
-run_command "cp ~/simple-hyprland/configs/hypr/hypridle.conf ~/.config/hypr/" "Copy Hypridle config" "yes" 
+run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/hypr/hypridle.conf /home/$SUDO_USER/.config/hypr/" "Copy Hypridle config" "yes"
 
 echo "------------------------------------------------------------------------"
