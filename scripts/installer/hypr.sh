a57ac87ad76d5fff25829415e22d9628f651090b
Gaurav Bankar <gaurav210233@gmail.com>
2024-08-08 09:07:21 +0530

    Added Recommendation

--- scripts/installer/hypr.sh
+++ scripts/installer/hypr.sh
@@ -8,6 +8,7 @@ source $SCRIPT_DIR/helper.sh
 
 log_message "Installation started for hypr section"
 print_info "\nStarting hypr setup..."
+print_info "\nEverything is recommended to INSTALL"
 
 run_command "pacman -S --noconfirm hyprland" "Install Hyprland (Must)" "yes"
 run_command "cp ~/simple-hyprland/configs/hypr/hyprland.conf ~/.config/hypr/" "Copy hyprland config (Must)" "yes" #no
@@ -16,7 +17,7 @@ run_command "pacman -S --noconfirm xdg-desktop-portal-hyprland" "Install XDG des
 
 run_command "pacman -S --noconfirm polkit-kde-agent" "Install KDE Polkit agent for authentication dialogs" "yes"
 
-run_command "pacman -S --noconfirm dunst" "Install Dunst notification daemon (Recommended)" "yes"
+run_command "pacman -S --noconfirm dunst" "Install Dunst notification daemon" "yes"
 run_command "cp -r ~/simple-hyprland/configs/dunst ~/.config/" "Copy dunst config" "yes"
 
 run_command "pacman -S --noconfirm qt5-wayland qt6-wayland" "Install QT support on wayland" "yes"
