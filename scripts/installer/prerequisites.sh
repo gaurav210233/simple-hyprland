01ee93ba43bf43c4fd0017298e3e7594a6e2b634
Gaurav Bankar <gaurav210233@gmail.com>
2024-08-08 12:34:47 +0530

    fixed yay

--- scripts/installer/prerequisites.sh
+++ scripts/installer/prerequisites.sh
@@ -11,9 +11,10 @@ print_info "\nStarting prerequisites setup..."
 
 run_command "pacman -Syyu --noconfirm" "Update package database and upgrade packages (Recommended)" "yes" # no
 
-run_command "pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd.. # builds with makepkg" "Install YAY (Must)/Breaks the script" "no" # no
-run_command "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd.. # builds with makepkg" "Build YAY (Must)/Breaks the script" "no" "no" # no
-
+if run_command "pacman -S --noconfirm --needed git base-devel" "Install YAY (Must)/Breaks the script" "yes"; then # 
+    run_command "git clone https://aur.archlinux.org/yay.git && cd yay" "Clone YAY (Must)/Breaks the script" "no" "no" 
+    run_command "makepkg --noconfirm -si && cd .. # builds with makepkg" "Build YAY (Must)/Breaks the script" "no" "no" 
+fi
 run_command "pacman -S --noconfirm pipewire wireplumber" "Configuring audio (Recommended)" "yes" 
 
 run_command "pacman -S --noconfirm ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono" "Installing Nerd Fonts and Symbols (Recommended)" "yes" 
