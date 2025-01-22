d6b281819b0c462994fd892f7ff4a8a5a9559437
Gaurav Bankar <gaurav210233@gmail.com>
2025-01-22 09:08:55 +0530

    added installation: pamixer and brightnessctl

--- scripts/installer/prerequisites.sh
+++ scripts/installer/prerequisites.sh
@@ -15,7 +15,7 @@ if run_command "pacman -S --noconfirm --needed git base-devel" "Install YAY (Mus
     run_command "git clone https://aur.archlinux.org/yay.git && cd yay" "Clone YAY (Must)/Breaks the script" "no" "no" 
     run_command "makepkg --noconfirm -si && cd .. # builds with makepkg" "Build YAY (Must)/Breaks the script" "no" "no" 
 fi
-run_command "pacman -S --noconfirm pipewire wireplumber" "Configuring audio (Recommended)" "yes" 
+run_command "pacman -S --noconfirm pipewire wireplumber pamixer brightnessctl" "Configuring audio and brightness (Recommended)" "yes" 
 
 run_command "pacman -S --noconfirm ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono" "Installing Nerd Fonts and Symbols (Recommended)" "yes" 
 
